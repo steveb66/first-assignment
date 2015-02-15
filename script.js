47bd01670f7b4377ae0382fc144b2031d65527ad
Steve Brown <steveb.brown@gmail.com>
2015-02-15 16:12:55 -0500

    push

---
+++ script.js
@@ -0,0 +1,9 @@
+// array
+var thingsToDo = ["run", "go to gym", "eat pizza", "go to cinema", "drink beer", "see a show", "eat brunch", "read a book"];
+
+
+// loop thru and log elements of array
+for (i = 0; i < thingsToDo.length; i++) {
+	console.log(thingsToDo[i]);
+}
+
